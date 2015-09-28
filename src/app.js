0d75796d7cd5698393a5acedefaef3f71fc93407
busayo <prosperotemuyiwa@gmail.com>
2015-09-28 07:07:18 +0100

    Refactor app.js to get data from .json file

--- src/app.js
+++ src/app.js
@@ -1,48 +1,5 @@
-var StatesAndCapital = {
-    'list' : function(){
+var statesAndCapital = require('./data.json');
 
-    	return {
-
-     	'Abia'        : 'Umuahia',
-     	'Adamawa'     : 'Yola',
-     	'Akwa-Ibom'   : 'Uyo',
-     	'Anambra'     : 'Akwa',
-     	'Bauchi'      : 'Bauchi',
-     	'Bayelsa'     : 'Yenagoa',
-     	'Benue'       : 'Makurdi',
-     	'Borno'       : 'Maiduguri',
-     	'Cross-River' : 'Calabar',
-		'Delta'	      : 'Asaba',
-		'Ebonyi'      :	'Abakaliki',
-		'Edo'         :	'Benin-City',
-		'Ekiti'       : 'Ado-Ekiti',
-		'Enugu'       :	'Enugu',
-		'FCT'         :	'Abuja',
-		'Gombe'       :	'Gombe',
-		'Imo'		  :	'Owerri',
-		'Jigawa'	  :	'Dutse',
-		'Kaduna'	  :	'Kaduna',
-		'Kano'		  : 'Kano',
-		'Katsina'	  :	'Katsina',
-		'Kebbi' 	  :	'Birnin-Kebbi',
-		'Kogi'  	  :	'Lokoja',
-		'Kwara' 	  :	'Ilorin',
-		'Lagos'       :	'Ikeja',
-		'Nassarawa'   :	'Lafia',
-		'Niger' 	  :	'Minna',
-		'Ogun'		  :	'Abeokuta',
-		'Ondo'  	  :	'Akure',
-		'Osun'  	  :	'Oshogbo',
-		'Oyo' 		  :	'Ibadan',
-		'Plateau'  	  :	'Jos',
-		'Rivers'  	  :	'Port-Harcourt',
-		'Sokoto'  	  :	'Sokoto',
-		'Taraba'  	  : 'Jalingo',
-		'Yobe'    	  :	'Damaturu',
-		'Zamfara' 	  : 'Gusau'
-	  };
-	}
-
+module.exports = {
+  all: statesAndCapital
 };
-
-module.exports = StatesAndCapital;
