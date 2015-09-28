3d79b43269ab4689800683643083cb9bccee2f50
busayo <prosperotemuyiwa@gmail.com>
2015-09-28 08:17:54 +0100

    Add spec and expectations

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,21 +1,16 @@
+require('jasmine-expect');
 var statesAndCapital = require('../src/app');
 
 describe('states and Capital', function() {
   describe('all', function() {
     it('should be a collection of strings in an object', function() {
-      expect(statesAndCapital.all).to.satisfy(isObjectOfStrings);
-
-      function isObjectOfStrings(array) {
-        return array.every(function(item) {
-          return typeof item === 'string';
-        });
-      }
+      expect(statesAndCapital.all).toBeObject();
     });
   });
 
   describe('Each City', function() {
     it('should return `Ikeja` as the Capital of `Lagos`', function() {
-      expect(statesAndCapital.all.Lagos).toEquals('Ikeja');
+      expect(statesAndCapital.all.Lagos).toEqual('Ikeja');
     });
   });
 });
